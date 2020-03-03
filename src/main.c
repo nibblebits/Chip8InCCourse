a755b644932b1bbc4eed9553bf6c62a83be81930
Your Name <you@example.com>
2020-03-03 18:03:19 +0000

    Made the emulator faster

--- src/main.c
+++ src/main.c
@@ -115,13 +115,13 @@ int main(int argc, char **argv)
         SDL_RenderPresent(renderer);
         if (chip8.registers.delay_timer > 0)
         {
-            Sleep(100);
+            Sleep(1);
             chip8.registers.delay_timer -=1;
         }
 
         if (chip8.registers.sound_timer > 0)
         {
-            Beep(15000, 100 * chip8.registers.sound_timer);
+            Beep(15000, 10 * chip8.registers.sound_timer);
             chip8.registers.sound_timer = 0;
         }
         
